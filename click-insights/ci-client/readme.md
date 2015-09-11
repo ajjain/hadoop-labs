d915fc396b0f8d981b4bec161ff8bea994b8399d
unknown <ajjain@IMPETUS-L284.impetus.co.in>
2015-09-11 19:51:24 +0530

    Add readme and update client generator

---
+++ click-insights/ci-client/readme.md
@@ -0,0 +1,3 @@
+## Client
+### FSA based event generator
+* Event trails represents real time user session with all activities on shopping site is generated. Also, generates patterns like browser refresh & back button press.
--- click-insights/ci-client/src/main/java/in/ajjain/ci/client/Client.java
+++ click-insights/ci-client/src/main/java/in/ajjain/ci/client/Client.java
@@ -11,21 +11,29 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class Client.
  */
 public class Client {
 
 	/** The location list. */
-	private List<List<String>> locationList = new ArrayList<>();
+	private static final List<List<String>> locationList = new ArrayList<>();
 
 	/** The user list. */
-	private List<List<String>> userList = new ArrayList<>();
+	private static final List<List<String>> userList = new ArrayList<>();
 
+	/** The singleton. */
+	private static Client singleton = new Client();
+	
+	public static Client getInstance(){
+		return singleton;
+	}
 	/**
 	 * Instantiates a new client.
 	 */
@@ -56,32 +64,71 @@ public class Client {
 		}
 	}
 
+	/**
+	 * Gets the random user session.
+	 *
+	 * @return the random user session
+	 */
 	public List<List<String>> getRandomUserSession(){
 		return null;
 	}
 
+	/** The Constant rand. */
 	private static final Random rand = new Random();
+
+	/** The Constant srand. */
 	private static final Random srand = new Random(543291341223L);
 
+	/** The Constant browsers. */
 	private static final String[] browsers = {"chrome", "firefox", "safari", "opera", "IE", "ME"};
 
-	public void generateEventsinFile(String location, Date date, int eventCounts) throws FileNotFoundException, UnsupportedEncodingException{
-		int uIndex = (int)(userList.size() * rand.nextFloat());
-		int lIndex = (int)(locationList.size() * rand.nextFloat());
-		List<String> uAttr = userList.get(uIndex);
-		List<String> lAttr = locationList.get(lIndex);
+	/**
+	 * Generate eventsin file.
+	 *
+	 * @param location the location
+	 * @param date the date
+	 * @param eventCounts the event counts
+	 */
+	public void generateEventsinFile(String location, Date date, int eventCounts) {
 		int totalCnt = 0;
-		PrintWriter writer = new PrintWriter(location, "UTF-8");
-		while(totalCnt < eventCounts){
-			List<EventBean> resultEvents = UserSessionGenerator.getInstance().generate(
-					""+srand.nextInt(), 
-					uAttr.get(2), uAttr.get(0), uAttr.get(1), 
-					lAttr.get(2), lAttr.get(2), lAttr.get(1), 
-					browsers[(int)(rand.nextFloat() * browsers.length)], 
-					new Date(date.getTime()+(int)(rand.nextFloat() * 1000*3600*24)), 
-					(int)(rand.nextFloat() * 15*60)
-					);
-			totalCnt = totalCnt + resultEvents.size();
+		try (PrintWriter writer = new PrintWriter(location)){
+			while(totalCnt < eventCounts){
+				
+				int uIndex = (int)(userList.size() * rand.nextFloat());
+				int lIndex = (int)(locationList.size() * rand.nextFloat());
+				List<String> uAttr = userList.get(uIndex);
+				List<String> lAttr = locationList.get(lIndex);
+				
+				List<EventBean> resultEvents = UserSessionGenerator.getInstance().generate(
+						""+Math.abs(srand.nextInt()), 
+						uAttr.get(2), uAttr.get(0), uAttr.get(1), 
+						lAttr.get(2), lAttr.get(2), lAttr.get(1), 
+						browsers[(int)(rand.nextFloat() * browsers.length)], 
+						new Date(date.getTime()+(int)(rand.nextFloat() * 1000*3600*24)), 
+						(int)(rand.nextFloat() * 15*60)
+						);
+				
+				for(EventBean bean: resultEvents){
+					writer.println(bean.toString());
+				}
+				
+				totalCnt = totalCnt + resultEvents.size();
+			}
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
 		}
+	}
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		c.set(2015, 9, 11, 00, 0, 0);
+		Date dt = c.getTime();
+		
+		Client.getInstance().generateEventsinFile("D:\\RnD\\hadoop-labs\\clicks.dat", dt, 100);
 	}
 }
